09a46ceea96371d7bc854dc5df06594122ba046b
Shaikhnbake <shaikhnajm21@gmail.com>
2022-10-30 20:39:57 -0400

    Fixed citysearch error
    Also started working on Show/Hide event details function

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,23 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 class Event extends Component {
+    
     render() {
-        return <div></div>;
+        const { event } = this.props;
+        const [show, toggleShow] = useState(false);
+        return (
+            <div className="Event">
+                <h1 className="summary">{event.summary}</h1>
+                {
+                    show?<p>{event.description}</p>:null
+                }
+                <button className="details" onClick={() => toggleShow(!show)}>
+                    More/Less details
+                </button>
+            </div>
+
+
+        );
     }
 }
 
--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -5,9 +5,10 @@ import { mockData } from "../mock-data";
 import { extractLocations } from '../api';
 
 describe('<CitySearch /> component', () => {
-    let CitySearchWrapper;
+    let locations, CitySearchWrapper;
     beforeAll(() => {
-        CitySearchWrapper = shallow(<CitySearch />);
+        locations = extractLocations(mockData);
+        CitySearchWrapper = shallow(<CitySearch locations={locations} />);
     });
 
     test('render text input', () => {
@@ -50,6 +51,13 @@ describe('<CitySearch /> component', () => {
             return location.toUpperCase().indexOf(query.toUpperCase()) > -1;
         });
         expect(CitySearchWrapper.state('suggestions')).toEqual(filteredLocations);
+    });
+
+    test('selecting a suggestion should change query state', () => {
+        CitySearchWrapper.setState({ query: 'Berlin' });
+        const suggestions = CitySearchWrapper.state('suggestions');
+        CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
+        expect(CitySearchWrapper.state('query')).toBe(suggestions[0]);
     });
 
 
