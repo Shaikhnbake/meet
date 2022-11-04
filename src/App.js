689f499a895ddace788fa9fb51478f7c3ba4f032
Shaikhnbake <shaikhnajm21@gmail.com>
2022-11-04 19:12:13 -0400

    Added api integration tests
    One test still giving error

--- src/App.js
+++ src/App.js
@@ -4,14 +4,43 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { mockData } from './mock-data';
+import { extractLocations, getEvents } from './api';
 
 class App extends Component {
+
+  state = {
+    events: [],
+    locations: []
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+      events :
+      events.filter((event) => event.location === location);
+      this.setState({ events: locationEvents });
+    });
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if(this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+
   render() {
     return (
       <div className='App'>
-        <CitySearch />
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents />
-        <EventList events={mockData}/>
+        <EventList events={this.state.events}/>
       </div>
     );
   }
