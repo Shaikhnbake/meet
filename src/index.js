82f6ad11b886ff71f6f4a728983896cf9c89c80a
Shaikhnbake <shaikhnajm21@gmail.com>
2022-11-15 00:43:16 -0500

    Tranformed app into a PWA
    Added icons and turned on service worker

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 atatus.config('5cdaea4fcf714956ad382452ce0d9f0e').install();
 
@@ -15,10 +16,20 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
+
+
+
+
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+
+// serviceWorkerRegistration.unregister(); UNCOMMENT OUT IF YOU WANT TO HAVE APP RUN ONLY ONLINE
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
